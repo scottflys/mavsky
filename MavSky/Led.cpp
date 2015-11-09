69e2e40eb2bb01ba34d123bd341ac1c6ac0260d3
scottflys <scottflys@watsys.com>
2015-11-09 18:54:29 -0500

    added led classes to handle flexible led patterns

--- MavSky/Led.cpp
+++ MavSky/Led.cpp
@@ -11,6 +11,105 @@ Led::Led(uint8_t pin, uint8_t led_count) {
   strip.begin();
   strip.show();                                     // all pixels off
   sequence_complete = 0;
+ 
+  LedPattern* led_pattern = new LedPattern((char*)"all_off"); 
+  LedStripState* strip_state = new LedStripState();
+  strip_state->add_bulbs(0, 0, 0, 8);
+  led_pattern->add_strip_state(strip_state);
+  add_pattern(led_pattern);
+
+  led_pattern = new LedPattern((char*)"normal"); 
+  strip_state = new LedStripState();
+  strip_state->add_bulb(255, 0, 0);
+  strip_state->add_bulbs(0, 0, 0, 6);
+  strip_state->add_bulb(0, 255, 0);
+  led_pattern->add_strip_state(strip_state);
+  add_pattern(led_pattern);
+
+  led_pattern = new LedPattern((char*)"landing1"); 
+  strip_state = new LedStripState();
+  strip_state->add_bulb(255, 0, 0);
+  strip_state->add_bulbs(255, 255, 255, 6);
+  strip_state->add_bulb(0, 255, 0);
+  led_pattern->add_strip_state(strip_state); 
+  add_pattern(led_pattern);
+  
+  // 0                    this are descending state sequences
+  // 1 R             G    
+  // 2 R-W         W-G
+  // 3 R-W-W-    W-W-G
+  // 4 R-W-W-W-W-W-W-G
+  led_pattern = new LedPattern((char*)"landing2"); 
+  strip_state = new LedStripState();
+  strip_state->add_bulbs(0, 0, 0, 8);
+  led_pattern->add_strip_state(strip_state);  
+
+  strip_state = new LedStripState();
+  strip_state->add_bulb(255, 0, 0);
+  strip_state->add_bulbs(0, 0, 0, 6);
+  strip_state->add_bulb(0, 255, 0);
+  led_pattern->add_strip_state(strip_state);  
+
+  strip_state = new LedStripState();
+  strip_state->add_bulb(255, 0, 0);
+  strip_state->add_bulb(255, 255, 255);
+  strip_state->add_bulbs(0, 0, 0, 4);
+  strip_state->add_bulb(255, 255, 255);
+  strip_state->add_bulb(0, 255, 0);
+  led_pattern->add_strip_state(strip_state);  
+
+  strip_state = new LedStripState();
+  strip_state->add_bulb(255, 0, 0);
+  strip_state->add_bulbs(255, 255, 255, 2);
+  strip_state->add_bulbs(0, 0, 0, 2);
+  strip_state->add_bulbs(255, 255, 255, 2);
+  strip_state->add_bulb(0, 255, 0);
+  led_pattern->add_strip_state(strip_state);
+    
+  strip_state = new LedStripState();
+  strip_state->add_bulb(255, 0, 0);
+  strip_state->add_bulbs(255, 255, 255, 6);
+  strip_state->add_bulb(0, 255, 0);
+  led_pattern->add_strip_state(strip_state);  
+  add_pattern(led_pattern);
+   
+}
+
+LedBulbState::LedBulbState(uint8_t red_param, uint8_t green_param, uint8_t blue_param) {
+  red = red_param;
+  green = green_param;
+  blue = blue_param;
+}
+
+LedStripState::LedStripState() {
+  
+}
+
+void LedStripState::add_bulb(uint8_t red_param, uint8_t green_param, uint8_t blue_param) {
+  bulbs[bulb_count] = new LedBulbState(red_param, green_param, blue_param);
+  bulb_count++;
+}
+
+void LedStripState::add_bulbs(uint8_t red_param, uint8_t green_param, uint8_t blue_param, uint8_t count) {
+  for(uint8_t i=0; i<count; i++) {
+    add_bulb(red_param, green_param, blue_param);
+  }
+}
+
+LedPattern::LedPattern(char* name_param) {
+  pattern_name = name_param;
+  LedBulbState* led_states[25];
+  strip_state_count = 0;
+}
+
+void LedPattern::add_strip_state(LedStripState* strip_state_param) {
+  led_strip_states[strip_state_count] = strip_state_param;
+  strip_state_count++;  
+}
+
+void Led::add_pattern(LedPattern* pattern) {
+  led_patterns[led_pattern_count] = pattern;
+  led_pattern_count++;
 }
 
 void Led::sequence_all_off() {
--- MavSky/Led.h
+++ MavSky/Led.h
@@ -3,20 +3,53 @@
 
 #include <WProgram.h> 
 #include <Adafruit_NeoPixel.h>
-  
+
+class LedBulbState {
+  public:
+    uint8_t red = 0;
+    uint8_t green = 0;
+    uint8_t blue = 0;
+    LedBulbState(uint8_t red_param, uint8_t green_param, uint8_t blue_param);
+};
+
+class LedStripState {
+  private:
+    uint8_t bulb_count = 0;
+    LedBulbState* bulbs[8];  
+
+  public:
+    void add_bulb(uint8_t red_param, uint8_t green_param, uint8_t blue_param);
+    void add_bulbs(uint8_t red_param, uint8_t green_param, uint8_t blue_param, uint8_t count);
+    LedStripState();
+};
+
+class LedPattern {
+  private:
+    LedStripState* led_strip_states[25];
+    uint8_t   strip_state_count = 0;
+    char*     pattern_name;
+  public:
+    LedPattern(char* name_param);
+    void add_strip_state(LedStripState* strip_state_param);
+};
+
 class Led {
   private:
     Adafruit_NeoPixel strip; 
+    LedPattern* led_patterns[25];
+    uint16_t led_pattern_count = 0;  
     uint8_t leds_on_strip = 0;
     int8_t state = 0;
     uint16_t state_count = 0;
     uint8_t  sequence_complete = 0;
     uint8_t previous_pattern = 0;
+
+    void add_pattern(LedPattern* pattern);
     void sequence_all_off();
     void sequence_normal();
     void sequence_landing1();
     void sequence_landing2(uint8_t descending);
-    
+
   public:
     Led(uint8_t pin, uint8_t led_count);
     void process_200_millisecond();
