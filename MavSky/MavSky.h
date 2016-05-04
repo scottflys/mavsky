5c6d945b76bfc8b0b0e1000e5a442e6a1b8b3eda
Scott Simpson <ssimpson@watsys.com>
2016-05-04 12:09:43 -0400

    updated version

--- MavSky/MavSky.h
+++ MavSky/MavSky.h
@@ -13,7 +13,7 @@
 #ifndef MAVSKY_H
 #define MAVSKY_H
 
-#define PRODUCT_STRING  "MAVSky Version 2.1.17.5"
+#define PRODUCT_STRING  "MAVSky Version 2.1.17.5a"
 
 #define FAULT_MAV_OFFLINE               1
 #define FAULT_SPORT_OFFLINE             2
